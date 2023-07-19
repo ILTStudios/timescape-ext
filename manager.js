e0be34ad00ede15cf096e8a1631548b109d4938c
Unknown <iltstudios69@gmail.com>
2023-07-19 21:12:57 +0100

    got update option for time setting working

--- manager.js
+++ manager.js
@@ -69,6 +69,8 @@ time_settings.addEventListener('click', () => {
     }
 
     time_settings.children[option_time].classList.add('selected');
+
+    update_options(option_time, "time")
 });
 
 format_settings.addEventListener('click', () => {
@@ -81,6 +83,54 @@ format_settings.addEventListener('click', () => {
     }
 
     format_settings.children[option_format].classList.add('selected');
+
+    update_options(option_format, "format")
 });
 
 //internal working
+function update_options(value, setting){
+    //time
+    if(setting == 'time'){
+        var time_value = document.querySelector('.todo .time input').value;
+        console.log(time_value);
+
+        if(value != 0){
+            //convert to 24
+            let [time, modifier] = time_value.split(' ');
+            let [hours, minutes] = time.split(':');
+            
+            if(hours === '12'){
+                hours = '00';
+            }if(modifier === 'PM'){
+                hours = parseInt(hours, 10) + 12;
+            }
+
+            var twenty_four_hour = `${hours}:${minutes}`;
+            document.querySelector('.todo .time > input').value = twenty_four_hour;
+        }
+        else{
+            //convert to 12
+            var hours = time_value.split(':')[0];
+            var minutes = time_value.split(':')[1];
+            
+            let AMorPM = 'AM';
+            if(hours>12)AMorPM = 'PM';
+            hours = (hours%12);
+
+            var twelve_hour = `${hours}:${minutes} ${AMorPM}`;
+            document.querySelector('.todo .time > input').value = twelve_hour;
+        }
+
+        if(value != 0){
+            option_time = 1;
+        }else{
+            option_time = 0;
+        }
+        return
+    }
+
+    //format
+    if(setting == 'format'){
+
+    }
+}
